8f0812e0f61c186c785dab8680fa1570a6aacf4d
gabyag <gabyaguil915@gmail.com>
2022-03-24 22:07:29 -0600

    Subiendo practica3

---
+++ practica3/laravelCrud/resources/views/pets/create.blade.php
@@ -0,0 +1,7 @@
+<h1> Nueva Mascota</h2>
+
+<form action="{{url('pet')}}" method="post">
+    @csrf
+    @include('pets.form')
+
+</form>
---
+++ practica3/laravelCrud/resources/views/pets/form.blade.php
@@ -0,0 +1,5 @@
+<label for="name">Nombre:</label>
+<input type="text" name="name" id="">
+<label for="age">Edad</label>
+<input type="text" name="age" id="">
+<button type="submit">Guardar</button>
