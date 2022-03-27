f590b6bfd32690715553e85c97bd99b128f31675
Gabrielag2 <98607696+Gabrielag2@users.noreply.github.com>
2022-03-26 22:02:11 -0600

    Delete practica3/laravelCrud directory

--- practica3/laravelCrud/resources/views/pets/create.blade.php
+++
@@ -1,7 +0,0 @@
-<h1> Nueva Mascota</h2>
-
-<form action="{{url('pet')}}" method="post">
-    @csrf
-    @include('pets.form')
-
-</form>
--- practica3/laravelCrud/resources/views/pets/form.blade.php
+++
@@ -1,5 +0,0 @@
-<label for="name">Nombre:</label>
-<input type="text" name="name" id="">
-<label for="age">Edad</label>
-<input type="text" name="age" id="">
-<button type="submit">Guardar</button>
